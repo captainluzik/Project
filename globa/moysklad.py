98a48843ec8a71a8aa4149c978a6cae3946bff9b
DL <dmitriy.luzanovsky@gmail.com>
2024-05-14 17:17:07 +0300

    hot fix

--- globa/moysklad.py
+++ globa/moysklad.py
@@ -96,7 +96,7 @@ class Moysklad:
                     percent_cashback = attribute['value']
             if percent_cashback > 0:
                 print(
-                    f'У товара "{product_obj['name']}" кешбек {percent_cashback}% та ціна {product['total_price']}')
+                    f'У товара {product_obj['name']} кешбек {percent_cashback}% та ціна {product['total_price']}')
                 total_cashback_sum = product['total_price'] * percent_cashback / 100
                 products_cashback.append(total_cashback_sum)
         if products_cashback:
