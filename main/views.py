67170f19a1eada561c0d2077741ccc202c7e8ed6
DL <dmitriy.luzanovsky@gmail.com>
2024-05-14 17:59:39 +0300

    hot fix

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from globa.moysklad import Moysklad
+import json
 
 
 @csrf_exempt
@@ -9,7 +10,8 @@ def webhook(request):
     print(request.body)
     if request.method == 'POST':
         print(request.body)
-        retaildemang_id = request.body['events'][0]['meta']['href'].split('/')[-1]
+        body = json.loads(request.body)
+        retaildemang_id = body['events'][0]['meta']['href'].split('/')[-1]
         ms = Moysklad()
         ms.edit_retaildemand_description(retaildemang_id)
         print(f'Edited {retaildemang_id}')
