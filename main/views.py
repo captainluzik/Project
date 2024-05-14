cb2213e0e1affcbe737cdc847cabc1a91a6d2a68
DL <dmitriy.luzanovsky@gmail.com>
2024-05-14 17:15:19 +0300

    change static conf

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from globa.moysklad import Moysklad
 
 
 @csrf_exempt
@@ -8,6 +9,10 @@ def webhook(request):
     print(request.body)
     if request.method == 'POST':
         print(request.body)
+        retaildemang_id = request.body['events'][0]['meta']['href'].split('/')[-1]
+        ms = Moysklad()
+        ms.edit_retaildemand_description(retaildemang_id)
+        print(f'Edited {retaildemang_id}')
         return JsonResponse({'status': 'ok'})
     if request.method == 'GET':
         print(request.GET)
